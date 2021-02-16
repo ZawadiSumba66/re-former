561c154a7300e1a63d1121282d59b9da6cfc30a1
KumarAmitt <amitt2015@outlook.com>
2021-02-16 14:50:07 +0530

    Implement HTML form with authenticity token and required fields

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ ruby '3.0.0'
 gem 'rails', '~> 6.1.2', '>= 6.1.2.1'
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3', '~> 1.4'
+gem 'pry-rails'
 # Use Puma as the app server
 gem 'puma', '~> 5.0'
 # Use SCSS for stylesheets
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,25 @@
 class UsersController < ApplicationController
-    def new
+
+  def new; end
+
+  def create
+    # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :new
     end
-    def create
-        
-    end
+
+  end
+
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
+
 end
