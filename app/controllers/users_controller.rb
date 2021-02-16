10caad74887bde69e519b3e914cc12231b85374e
KumarAmitt <amitt2015@outlook.com>
2021-02-16 16:19:23 +0530

    Add edit functionality and error messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,20 @@ class UsersController < ApplicationController
 
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to new_user_path
+    else
+      render :edit
+    end
+  end
+
 
 
   private
